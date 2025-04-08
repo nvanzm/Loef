87c38d954c984574b98ee69723dc1634e5db3cb8
nvanzm <noach.ambachtsheer@gmail.com>
2025-04-08 11:49:55 +0200

    Ja lol

--- LoefApp/src/main/java/com/example/loef/Startup.java
+++ LoefApp/src/main/java/com/example/loef/Startup.java
@@ -10,9 +10,7 @@ import java.io.IOException;
 public class Startup extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-
-//        FXMLLoader fxmlLoader = new FXMLLoader(Startup.class.getResource("werknemer/werknemer-uren-screen.fxml"));
-        FXMLLoader fxmlLoader = new FXMLLoader(Startup.class.getResource("werkgever/werkgever-uren-screen.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Startup.class.getResource("login-screen.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Loef");
